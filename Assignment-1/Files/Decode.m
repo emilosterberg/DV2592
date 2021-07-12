95f6d61c5a9b700a17e635ea11d354898d78c434
emilosterberg <75029993+emilostkth@users.noreply.github.com>
2021-07-12 09:44:26 +0200

    Create Decode.m

---
+++ Assignment-1/Files/Decode.m
@@ -0,0 +1,15 @@
+function Sec = Decode(Stego, k)
+% Where the inputs are:
+% stego: is the RGB image containing the embeded randomised data
+% k: a secret key for restoration can be a string 'myPassWord2020!' or a digit 21545422, choosing any option is fine with me
+
+% The output, Sec: The secret binary image of the same dimensions as Im
+% Example use:
+% Sec = Decode ( 'Stego.png', 'myPassWord2020!')
+% 
+
+
+
+
+
+end
