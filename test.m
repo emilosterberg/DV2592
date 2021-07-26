d3c3d0b5826e712df375df5e0c3e8c4d7de8ef33
emilosterberg <75029993+emilostkth@users.noreply.github.com>
2021-07-26 16:50:49 +0200

    Update test.m

--- test.m
+++ test.m
@@ -1,2 +1,42 @@
 % Test file
 clc;clear
+rgb = imread('coloredChips.png');
+imshow(rgb)
+%%
+d = drawline;
+%%
+pos = d.Position;
+diffPos = diff(pos);
+diameter = hypot(diffPos(1),diffPos(2))
+
+%%
+
+gray_image = rgb2gray(rgb);
+imshow(gray_image)
+
+%%
+clc
+[centers,radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark', ...
+    'Sensitivity',0.95)
+
+%%
+imshow(rgb)
+h = viscircles(centers,radii);
+%%
+[centers,radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark', ...
+          'Sensitivity',0.95,'Method','twostage');
+
+delete(h)
+h = viscircles(centers,radii);
+
+%%
+% Test file
+clc;clear
+rgb = imread('coloredChips.png');
+imshow(rgb)
+
+%%
+gray_image = rgb2gray(rgb);
+J = imcomplement(rgb);
+figure()
+imshow(J)
